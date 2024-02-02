75707bc4789f2da53820d2954c3d069ba956b232
Chase Hartsell <cmhartsell@me.com>
2024-02-02 15:15:43 -0600

    Refactor and add methods for recoloring images

--- image.py
+++ image.py
@@ -1,27 +1,62 @@
 from PIL import Image, ImageSequence
 from io import BytesIO
 from base64 import b64encode
+import numpy as np
 
 
 def resize(img_path: str, size=(16, 16)) -> str:
-    frames = ImageSequence.Iterator(Image.open(img_path))
+    frames = thumbnails(Image.open(img_path))
 
-    # Resize each image individually
+    # Convert the image to a buffer
+    buffer = frames_to_buffer(frames)
+
+    # Return a base64 encoded string
+    return b64encode(buffer.getvalue())
+
+
+def thumbnails(img, size=(16, 16)):
+    frames = ImageSequence.Iterator(img)
+
     thumbnails = []
     for frame in frames:
-        frame = frame.resize(size, Image.NEAREST).convert("RGB")
-        thumbnails.append(frame)
+        frame = frame.resize(size, Image.NEAREST)
+        thumbnail = frame.convert("RGB")
+        thumbnails.append(thumbnail)
+    return thumbnails
 
+
+def recolor(img: Image, original: tuple[int], new: tuple[int]) -> Image:
+    frames = thumbnails(img)
+
+    new_frames = []
+    for frame in frames:
+        frame_data = np.array(frame)
+
+        # Replace occurrences of the original color with the new color
+        r, g, b = frame_data.T
+        replaceable_areas = (r == original[0]) & (g == original[1]) & (b == original[2])
+        frame_data[..., :][replaceable_areas.T] = new
+
+        new_frame = Image.fromarray(frame_data)
+        new_frame.info = frame.info
+        new_frames.append(new_frame)
+
+    buffer = frames_to_buffer(new_frames)
+    return Image.open(buffer, formats=["WEBP"])
+
+
+def frames_to_buffer(frames) -> BytesIO:
     # Save the image to a buffer
     buffer = BytesIO()
     first_frame_duration = (
-        thumbnails[0].info["duration"] if "duration" in thumbnails[0].info else 0
+        frames[0].info["duration"] if "duration" in frames[0].info else 0
     )
-    if len(thumbnails) > 1:
-        thumbnails[0].save(
+
+    if len(frames) > 1:
+        frames[0].save(
             buffer,
             save_all=True,
-            append_images=thumbnails[1:],
+            append_images=frames[1:],
             duration=first_frame_duration,
             loop=0,
             format="WEBP",
@@ -29,7 +64,7 @@ def resize(img_path: str, size=(16, 16)) -> str:
             quality=100,
         )
     else:
-        thumbnails[0].save(
+        frames[0].save(
             buffer,
             save_all=True,
             duration=first_frame_duration,
@@ -39,5 +74,4 @@ def resize(img_path: str, size=(16, 16)) -> str:
             quality=100,
         )
 
-    # Return a base64 encoded string
-    return b64encode(buffer.getvalue())
+    return buffer
